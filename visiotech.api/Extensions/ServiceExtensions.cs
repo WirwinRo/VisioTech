30bb9c9cfcff9c63e7ddb3dc831721490c3135bc
Edwin Romero <romero.espinoza@gmail.com>
2025-02-14 14:12:03 +0100

    Application y configuracion de inyeccion de dependencias

---
+++ visiotech.api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using visiotech.application.Application;
+using visiotech.application.Interfaces;
+using visiotech.domain.Interfaces;
+using visiotech.infrastructure.Repositories;
+
+namespace visiotech.api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddInjectiionRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IGrapeRepository, GrapeRepository>();
+            services.AddScoped<IGrapeApp, GrapeApp>();
+           
+        }
+    }
+}
--- visiotech.api/Program.cs
+++ visiotech.api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using visiotech.api.Extensions;
 using visiotech.infrastructure.Config;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,9 @@ builder.Services.AddDbContext<VisioTechContext>(opt =>
 builder.Services.AddScoped<DbContextFactory>();
 
 builder.Services.AddAutoMapper(typeof(Program));
+
+//Inyecvción de dependencias
+builder.Services.AddInjectiionRepositories();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
